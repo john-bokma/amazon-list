6ea5d58a217b6ead70dd96131967a8b64e53c56f
John Bokma <contact@johnbokma.com>
2017-03-08 19:24:26 -0600

    Add code to print the result formatted

--- listitems.pl
+++ listitems.pl
@@ -3,8 +3,11 @@
 use strict;
 use warnings;
 
+use Text::Wrap qw(wrap $columns $huge);
+
 use lib 'lib';
 use Amazon::List;
+
 my $url = shift;
 if ( ! defined $url ) {
     print <<'USAGE_END';
@@ -17,3 +20,38 @@ USAGE_END
     exit 1;
 }
 
+my $items = Amazon::List->new()->get_items( $url );
+if ( ! @$items ) {
+    print "No items on Amazon list $url\n";
+    exit;
+}
+
+binmode STDOUT, ':encoding(UTF-8)';
+my $sum = 0;
+my $width = 76;
+my $item_no = 1;
+
+for my $item ( @$items ) {
+
+    my $bullet = sprintf '%3d.  ', $item_no;
+    my $price  = sprintf '  %7s',
+	defined $item->{ price } ? $item->{ price } : '-';
+    my $description = "$item->{ title } $item->{ authors }";
+
+    $columns = $width - length $price;
+    my @description_lines = split /\n/, wrap(
+	$bullet,
+	' ' x length $bullet,
+	$description
+    );
+
+    my $first_line = shift @description_lines;
+    printf "%-*s%s\n", $columns, $first_line, $price;
+    print map { "$_\n" } @description_lines;
+
+    $sum += $item->{ price } if defined $item->{ price };
+    $item_no++;
+}
+
+my $total_line = sprintf "Total price: %.2f", $sum;
+printf "\n%*s\n", $width, $total_line;
