e469da15136c0ffe4df0610811062d0adc707c1f
John Bokma <contact@johnbokma.com>
2017-03-08 19:20:37 -0600

    Add a method to extract a single item from a Amazon list

--- lib/Amazon/List.pm
+++ lib/Amazon/List.pm
@@ -43,4 +43,36 @@ sub _get_html_tree {
     my $content = $response->decoded_content();
     return HTML::TreeBuilder->new_from_content( $content );
 }
+
+sub _extract_item {
+
+    my ( $self, $tree ) = @_;
+
+    my %item;
+
+    # title and author(s)
+    my $div = $tree->look_down( _tag => 'div', class => 'a-row a-size-small' );
+    defined $div or return;
+    # title
+
+    my $h5 = $div->look_down( _tag => 'h5' );
+    defined $h5 or return;
+    $item{ title } = $h5->as_trimmed_text();
+    $h5->delete();
+
+    # author(s)
+    $item{ authors } = $div->as_trimmed_text();
+
+    # price
+    my $span = $tree->look_down( _tag => 'span', id => qr/^itemPrice_/ );
+    if ( defined $span ) {
+	# Only extract the digits. For USA prices this gets rid of the $
+	( $item{ price } ) = $span->as_trimmed_text() =~ /([0-9.]+)/;
+    }
+    else {
+	$item{ price } = undef;
+    }
+    return \%item;
+}
+
 1;
