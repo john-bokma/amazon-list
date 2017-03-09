6f8a0164a7e6411301e1163424dcb3ae77824f17
John Bokma <contact@johnbokma.com>
2017-03-08 19:18:20 -0600

    Add a constructor and create a user agent object

--- lib/Amazon/List.pm
+++ lib/Amazon/List.pm
@@ -3,4 +3,26 @@ package Amazon::List;
 use strict;
 use warnings;
 
+use LWP::UserAgent;
+sub new {
+
+    my $class = shift;
+
+    my $self = bless {}, $class;
+    $self->_init_ua();
+    return $self;
+}
+
+sub _init_ua {
+
+    my $self = shift;
+
+    my $ua = LWP::UserAgent->new();
+
+    # Tell server we can handle compressed content (faster download)
+    $ua->default_header( 'Accept-Encoding' => 'gzip, deflate' );
+    $self->{ ua } = $ua;
+    return;
+}
+
 1;
