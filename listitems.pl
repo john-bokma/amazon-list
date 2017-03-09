27f36c5c18a6f66c880c87fecf439367e10eb2e2
John Bokma <contact@johnbokma.com>
2017-03-08 19:23:25 -0600

    Add code to obtain the first argument, show usage if missing

--- listitems.pl
+++ listitems.pl
@@ -5,3 +5,15 @@ use warnings;
 
 use lib 'lib';
 use Amazon::List;
+my $url = shift;
+if ( ! defined $url ) {
+    print <<'USAGE_END';
+Usage
+
+   perl listitems.pl URL
+
+with URL an Amazon List URL
+USAGE_END
+    exit 1;
+}
+
